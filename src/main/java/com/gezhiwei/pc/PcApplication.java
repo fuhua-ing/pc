39e5dfe5ccb0695c0476e8ed769859435bdf2270
葛志伟 <gezhiwei@jiangduoduo.com>
2018-08-09 10:14:47 +0800

    first commit

---
+++ src/main/java/com/gezhiwei/pc/PcApplication.java
@@ -0,0 +1,12 @@
+package com.gezhiwei.pc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcApplication.class, args);
+	}
+}
